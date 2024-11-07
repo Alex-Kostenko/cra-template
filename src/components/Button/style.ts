78a9f4b47c3f0df6558375fdc95297cdeb7e18ff
Vasyl Kharchenko <vkharchenko@classwallet.com>
2024-11-07 10:45:10 +0200

    Add fix for button.

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -4,6 +4,11 @@ const ButtonStyled = styled.button`
   color: white;
   background-color: orange;
   border-radius: 5px;
+
+  &:disabled {
+    background-color: #cccccc;
+    cursor: not-allowed;
+  }
 `;
 
 export {ButtonStyled};
